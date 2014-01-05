a5f4495781978e6d0253da58f47d576ba27531f4
phuvf <phuvf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-05 16:13:45 +0000

    V6 fix
    git-svn-id: https://plugins.svn.wordpress.org/smartbroker/trunk@833357 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- smartbroker.php
+++ smartbroker.php
@@ -3,7 +3,7 @@
 Plugin Name: SmartBroker
 Plugin URI: http://www.smart-broker.co.uk
 Description: A plugin to insert SmartBroker data into a Wordpress site
-Version: 4.0
+Version: 6.0
 Author: Nick Roberts
 Author URI: http://www.smart-broker.co.uk
 License: GPL2
